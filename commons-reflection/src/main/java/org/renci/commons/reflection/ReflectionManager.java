1f5171bbc0d9269df31f5bb5696b929daad0b7aa
Jason Reilly <jdr0887@renci.org>
2012-12-13 14:25:13 -0500

    incrementing commons-lang

--- commons-reflection/src/main/java/org/renci/commons/reflection/ReflectionManager.java
+++ commons-reflection/src/main/java/org/renci/commons/reflection/ReflectionManager.java
@@ -16,7 +16,7 @@ import java.util.jar.JarFile;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 
