f206ff9854abdc1b7a9496294f9e05aa4d25f896
Jason Reilly <jdr0887@gmail.com>
2016-03-09 14:11:00 -0500

    misc

--- commons-exec/src/main/java/org/renci/common/exec/Executor.java
+++ commons-exec/src/main/java/org/renci/common/exec/Executor.java
@@ -1,18 +1,19 @@
 package org.renci.common.exec;
 
 import java.io.File;
+import java.util.Map;
 
 /**
  * 
  * @author jdr0887
- *
+ * 
  */
 public interface Executor {
 
     /**
-     * run the command
      * 
-     * @return exit code
+     * @param input
+     * @return
      * @throws ExecutorException
      */
     public CommandOutput execute(CommandInput input) throws ExecutorException;
@@ -20,10 +21,12 @@ public interface Executor {
     /**
      * 
      * @param input
+     * @param substitutionMap
      * @param sources
      * @return
      * @throws ExecutorException
      */
-    public CommandOutput execute(CommandInput input, File... sources) throws ExecutorException;
+    public CommandOutput execute(CommandInput input, Map<String, String> substitutionMap, File... sources)
+            throws ExecutorException;
 
 }
