2c420c60834e4bc59f78c241f76e33ff81e73f79
Jason Reilly <jdr0887@gmail.com>
2016-03-09 14:10:41 -0500

    adding toString()

--- commons-exec/src/main/java/org/renci/common/exec/CommandInput.java
+++ commons-exec/src/main/java/org/renci/common/exec/CommandInput.java
@@ -46,94 +46,59 @@ public class CommandInput implements Serializable {
         this.exitImmediately = exitImmediately;
     }
 
-    /**
-     * @return the workDir
-     */
     public File getWorkDir() {
         return workDir;
     }
 
-    /**
-     * @param workDir
-     *            the workDir to set
-     */
     public void setWorkDir(File workDir) {
         this.workDir = workDir;
     }
 
-    /**
-     * @return the environment
-     */
     public Map<String, String> getEnvironment() {
         return environment;
     }
 
-    /**
-     * @param environment
-     *            the environment to set
-     */
     public void setEnvironment(Map<String, String> environment) {
         this.environment = environment;
     }
 
-    /**
-     * @return the command
-     */
     public String getCommand() {
         return command;
     }
 
-    /**
-     * @param command
-     *            the command to set
-     */
     public void setCommand(String command) {
         this.command = command;
     }
 
-    /**
-     * @return the maxRunTime
-     */
     public long getMaxRunTime() {
         return maxRunTime;
     }
 
-    /**
-     * @param maxRunTime
-     *            the maxRunTime to set
-     */
     public void setMaxRunTime(long maxRunTime) {
         this.maxRunTime = maxRunTime;
     }
 
-    /**
-     * @return the stdin
-     */
     public StringBuffer getStdin() {
         return stdin;
     }
 
-    /**
-     * @param stdin
-     *            the stdin to set
-     */
     public void setStdin(StringBuffer stdin) {
         this.stdin = stdin;
     }
 
-    /**
-     * @return the exitImmediately
-     */
     public Boolean getExitImmediately() {
         return exitImmediately;
     }
 
-    /**
-     * @param exitImmediately
-     *            the exitImmediately to set
-     */
     public void setExitImmediately(Boolean exitImmediately) {
         this.exitImmediately = exitImmediately;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "CommandInput [workDir=%s, environment=%s, command=%s, exitImmediately=%s, maxRunTime=%s, stdin=%s]",
+                workDir, environment, command, exitImmediately, maxRunTime, stdin);
     }
 
 }
